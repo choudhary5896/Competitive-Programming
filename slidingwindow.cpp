3b430eb955e4dbcf4e7d17e1fe90985108d1a9d1
KAILASH <72955726+choudhary5896@users.noreply.github.com>
2021-03-28 15:48:36 +0530

    Update slidingwindow.cpp

--- slidingwindow.cpp
+++ slidingwindow.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int maxSum(int arr[], int n, int k)
 {
@@ -18,9 +18,13 @@ int maxSum(int arr[], int n, int k)
 }
 int main()
 {
-	int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
-	int k = 4;
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	cin>>arr[i];
+	int k;
+	cin>>k;
 	cout << maxSum(arr, n, k);
 	return 0;
 }
